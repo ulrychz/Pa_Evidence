6be5793ced61b7eb83b8908681ac69cae1d52672
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-04-19 18:49:51 +0200

    Přidejte soubory projektu.

---
+++ Pa_Evidence/Models/Polozka.cs
@@ -0,0 +1,43 @@
+﻿namespace Pa_Evidence.Models
+{
+    public class Polozka
+    {
+        private int naklady = 10;
+        private int vynosy;
+
+        public Polozka() 
+        {
+            Datum = DateOnly.FromDateTime(DateTime.Today);
+        }
+        public Polozka(DateOnly datum, int naklady, int vynosy)
+        {
+            Datum = datum;
+            Naklady = naklady;
+            Vynosy = vynosy;
+        }
+        public DateOnly Datum { get; set; }
+        public int Naklady
+        {
+            get => naklady; set
+            {
+                naklady = Math.Abs(value);
+            }
+        }
+        public int Vynosy
+        {
+            get => vynosy; set
+            {
+                if (value < 0) 
+                {
+                    vynosy = 0;
+                }
+                else
+                {
+                    vynosy = value;
+                }
+            }
+        }
+
+        public int Zisk => Vynosy - Naklady;
+    }
+}
---
+++ Pa_Evidence/Pages/EvidenceV0.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace Pa_Evidence.Pages
+{
+    public partial class EvidenceV0
+    {
+        public Models.Polozka Polozka { get; set; } = new Models.Polozka();
+        
+        public List<Models.Polozka> PolozkyList { get; set; } = new List<Models.Polozka>();
+        
+        private void Pridat(Microsoft.AspNetCore.Components.Web.MouseEventArgs e)
+        {
+            //PolozkyList.Add(Polozka);
+            PolozkyList.Add(new Models.Polozka(Polozka.Datum, Polozka.Naklady, Polozka.Vynosy));
+        }
+    }
+}
